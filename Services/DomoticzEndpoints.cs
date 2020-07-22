f8eaed511664ba9c11eb8b40c8ab0bf6bf157772
Dominik Gogola <d.s.gogola@gmail.com>
2020-07-22 13:36:07 +0200

    - added domoticz endpoint classes

---
+++ Services/DomoticzEndpoints.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MalinkaSerwer.Services
+{
+    public class DomoticzEndpoints
+    {
+    }
+}
