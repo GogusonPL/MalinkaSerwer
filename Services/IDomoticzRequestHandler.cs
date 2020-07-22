f792d750a8a36221112c7211b2e7fa7eb46fff19
Dominik Gogola <d.s.gogola@gmail.com>
2020-07-22 13:54:19 +0200

    - updated interface

--- Services/IDomoticzRequestHandler.cs
+++ Services/IDomoticzRequestHandler.cs
@@ -1,12 +1,13 @@
 ﻿using MalinkaSerwer.Models;
+using System.Threading.Tasks;
 
 namespace MalinkaSerwer.Services
 {
     public interface IDomoticzRequestHandler
     {
-        public DomoticzResponse GetCurrentInfo();
-        public SimpleResult SetLight(bool isWorking);
-        public SimpleResult SetAc(bool isWorking);
+        public Task<DomoticzResponse> GetCurrentInfo();
+        public Task<SimpleResult> SetLight(bool isWorking);
+        public Task<SimpleResult> SetAc(bool isWorking);
 
     }
 }
