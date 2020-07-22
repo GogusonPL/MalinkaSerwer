f78bc1513ea9879604866d639ee177c13f58f0a6
Dominik Gogola <d.s.gogola@gmail.com>
2020-07-22 13:53:59 +0200

    - added logic for requests

--- Services/DomoticzRequestHandler.cs
+++ Services/DomoticzRequestHandler.cs
@@ -1,23 +1,74 @@
 ﻿using MalinkaSerwer.Models;
+using Newtonsoft.Json;
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MalinkaSerwer.Services
 {
     public class DomoticzRequestHandler : IDomoticzRequestHandler
     {
-        public DomoticzResponse GetCurrentInfo()
+        public async Task<DomoticzResponse> GetCurrentInfo()
         {
-            throw new NotImplementedException();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                var request = new HttpRequestMessage(HttpMethod.Post, DomoticzEndpoints.GetDataEndpoint);
+                var response = await client.SendAsync(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<DomoticzResponse>(jsonString);
+                }
+                return null;
+            }
         }
 
-        public SimpleResult SetAc(bool isWorking)
+        public async Task<SimpleResult> SetAc(bool isWorking)
         {
-            throw new NotImplementedException();
+            string endpoint = "";
+            if (isWorking)
+                endpoint = DomoticzEndpoints.AcOnEndpoint;
+            else
+                endpoint = DomoticzEndpoints.AcOffEndpoint;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                var request = new HttpRequestMessage(HttpMethod.Post, endpoint);
+                var response = await client.SendAsync(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<SimpleResult>(jsonString);
+                }
+                return null;
+            }
         }
 
-        public SimpleResult SetLight(bool isWorking)
+        public async Task<SimpleResult> SetLight(bool isWorking)
         {
-            throw new NotImplementedException();
+            string endpoint = "";
+            if (isWorking)
+                endpoint = DomoticzEndpoints.LighOnEndpoint;
+            else
+                endpoint = DomoticzEndpoints.LightOffEndpoint;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                var request = new HttpRequestMessage(HttpMethod.Post, endpoint);
+                var response = await client.SendAsync(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<SimpleResult>(jsonString);
+                }
+                return null;
+            }
         }
     }
 }
