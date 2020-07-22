81e549a34eabaea6c32375dca3aee4923044e031
Dominik Gogola <d.s.gogola@gmail.com>
2020-07-22 13:54:07 +0200

    - updated mock

--- Services/DomoticzRequestHandlerMock.cs
+++ Services/DomoticzRequestHandlerMock.cs
@@ -1,34 +1,20 @@
 ﻿using MalinkaSerwer.Models;
 using System.IO;
-using System.Timers;
+using System.Threading.Tasks;
 
 namespace MalinkaSerwer.Services
 {
     public class DomoticzRequestHandlerMock : IDomoticzRequestHandler
     {
-        Timer timer;
-        public DomoticzRequestHandlerMock()
-        {
-            timer = new Timer();
-            timer.Elapsed += CheckSmartHome;
-            timer.Interval = 10000;
-            timer.Start();
 
-        }
-
-        private void CheckSmartHome(object sender, ElapsedEventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public DomoticzResponse GetCurrentInfo()
+        public async Task<DomoticzResponse> GetCurrentInfo()
         {
             var jsonData = File.ReadAllText("domoticzResponse.json");
             var result = Newtonsoft.Json.JsonConvert.DeserializeObject<DomoticzResponse>(jsonData);
             return result;
         }
 
-        public SimpleResult SetAc(bool isWorking)
+        public async Task<SimpleResult> SetAc(bool isWorking)
         {
             var jsonData = File.ReadAllText("simple.json");
             var result = Newtonsoft.Json.JsonConvert.DeserializeObject<SimpleResult>(jsonData);
@@ -36,7 +22,7 @@ namespace MalinkaSerwer.Services
             return result;
         }
 
-        public SimpleResult SetLight(bool isWorking)
+        public async Task<SimpleResult> SetLight(bool isWorking)
         {
             var jsonData = File.ReadAllText("simple2.json");
             var result = Newtonsoft.Json.JsonConvert.DeserializeObject<SimpleResult>(jsonData);
